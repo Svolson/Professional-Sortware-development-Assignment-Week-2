d113068da0ed835713d98e2f479b110fc0625e7a
Svolson <93822517+Svolson@users.noreply.github.com>
2021-11-14 20:19:36 +0200

    Updated
    Forgot to save added Grade table in Mysql workshop. Add it via github

--- college database.sql
+++ college database.sql
@@ -1,11 +1,18 @@
 create database college;
 use college;
 create table student(Firstname char(50),
-					 Surname char(50),
+		     Surname char(50),
                      Algebra double,
                      Calculus double,
                      Programming double,
                      Dattabase double);
 
-select * from student 
+select * from student;
+
+create table grades(Firstname char(50),
+		    Surname char(50),
+                    Average double, 
+                    Grade double);
+
+select * from grades 
 
